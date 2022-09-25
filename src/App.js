4cc504212755e21284cd4d40ea08c126210af846
Mento Mume <akpankevman@gmail.com>
2022-09-25 13:38:12 +0100

    added private route, spinner and profile page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { ToastContainer } from "react-toastify";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
 import NavBar from "./components/NavBar";
+import PrivateRoute from "./components/PrivateRoute";
 import Explore from "./pages/Explore";
 import Offers from "./pages/Offers";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import ForgotPassword from "./pages/ForgotPassword";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -14,13 +17,16 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
-          <Route path="/profile" element={<SignIn />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/Forgot-password" element={<ForgotPassword />} />
         </Routes>
         <NavBar />
       </Router>
+      <ToastContainer />
     </>
   );
 }
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,4 +1,86 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
+import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 function SignIn() {
-  return <div>Sign In</div>;
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({});
+  const { email, password } = formData;
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      if (userCredential.user) {
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error("invalid user credentials");
+    }
+  };
+  return (
+    <div className="pageContainer">
+      <header>
+        <p className="pageHeader">Welcome Back!</p>
+      </header>
+      <form onSubmit={onSubmit}>
+        <input
+          type="email"
+          className="emailInput"
+          placeholder="Email"
+          id="email"
+          value={email}
+          onChange={onChange}
+        />
+        <div className="passwordInputDiv">
+          <input
+            type={showPassword ? "text" : "password"}
+            className="passwordInput"
+            placeholder="Password"
+            id="password"
+            value={password}
+            onChange={onChange}
+          />
+          <img
+            src={visibilityIcon}
+            className="showPassword"
+            alt="show password"
+            onClick={() => setShowPassword((prevState) => !prevState)}
+          />
+          <Link to="/forgot-password" className="forgotPasswordLink">
+            Forgot Password
+          </Link>
+
+          <div className="signInBar">
+            <p className="signInText">Sign In</p>
+            <button className="signInButton">
+              <ArrowRightIcon fill="#ffffff" width="34px" height="34px" />
+            </button>
+          </div>
+        </div>
+      </form>
+
+      <Link to="/sign-up" className="registerLink">
+        Sign Up Instead
+      </Link>
+    </div>
+  );
 }
 export default SignIn;
